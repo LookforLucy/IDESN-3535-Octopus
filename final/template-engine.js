0dd0c39749440a8c32435b4bf5c157912ad6dca2
LookforLucy <31733386+LookforLucy@users.noreply.github.com>
2017-12-07 10:11:28 -0500

    added class 8
    template

---
+++ final/template-engine.js
@@ -0,0 +1,21 @@
+const testString = "<div>I own a {{animal}}.</div>";
+// In order to change "animal" to whatever you want without manually having to do it, you add double curly brackets
+const testContext = {animal: "dog", age: 7};
+// {} are objects, [] are arrays
+function replaceString(string, key, value){
+  const searchTerm = "{{"+key+"}}";
+  return string.replace(searchTerm, value);
+// "searchTerm" searches for "animal"
+}
+function processTemplate(template, context) {
+Object.keys(context).forEach(function(key){
+  template = replaceString(template, key, context[key]);
+});
+return template;
+}
+
+function getTemplate(template, context) {
+  // taking an HTML object to prepend or append
+  return $(processTemplate(template, context));
+  //wrapping anything in jquery will turn it into HTML
+}
